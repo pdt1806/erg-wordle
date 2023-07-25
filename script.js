1701eb58df2a458428f439475b739c91b88f651f
Benny Nguyen <nmphuc1806@gmail.com>
2023-07-24 21:51:34 -0700

    Better details

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ console.log(`
 If you're interested in the source code, you can find it here:
 https://github.com/pdt1806/erg-wordle
 
-Please don't cheat using this console, the game is more fun if you don't!
+Please don't cheat using this console, it ruins the fun!
 `);
 
 const unfocus = () => {
