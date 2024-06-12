769f982873b4b072d72caeaef908c58399482937
pdt1806 <nmphuc1806@gmail.com>
2024-06-11 17:01:39 -0700

    fix: :bug: default theme = light

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,5 +1,5 @@
 const toggleTheme = () => {
-  const theme = localStorage.getItem("theme") === "dark" ? "light" : "dark";
+  const theme = localStorage.getItem("theme") === "light" ? "dark" : "light";
   document.getElementById("themeStyle").href = `styles/${theme}.css`;
   document.getElementById("themeIcon").src =
     theme === "light" ? "lib/sun.svg" : "lib/moon.svg";
@@ -7,7 +7,8 @@ const toggleTheme = () => {
 };
 
 const setTheme = () => {
-  const theme = localStorage.getItem("theme") || "dark";
+  const theme = localStorage.getItem("theme") || "light";
+  localStorage.setItem("theme", theme);
   document.getElementById("themeStyle").href = `styles/${theme}.css`;
   document.getElementById("themeIcon").src =
     theme === "light" ? "lib/sun.svg" : "lib/moon.svg";
